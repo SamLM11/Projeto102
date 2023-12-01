d7d2af6cca86800e7a52cfcbf073cde708db98e2
SamLM11 <samuellm2011@gmail.com>
2023-12-01 15:22:06 -0300

    first commit

---
+++ Move_File.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+from_dir = 'C:/Users/samue/Downloads'
+to_dir = 'C:/Users/samue/OneDrive/Documentos/Imagem-Aula102'
+
+list_of_files = os.listdir(from_dir)
+
+#print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    if extension == '':
+        continue
+    if extension in ['.gif', '.png', '.jpg', '.jpeg', '.jfif']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' + "Imagem-Aula102"
+        path3 = to_dir + '/' + "Imagem-Aula102" + '/' + file_name
+        if os.path.exists(path2):
+            print(f'movendo {file_name} ...')
+            shutil.move(path1,path3)
+        else:
+            os.mkdir(path2)
+            print(f'movendo {file_name} ...')
+            shutil.move(path1,path3)
